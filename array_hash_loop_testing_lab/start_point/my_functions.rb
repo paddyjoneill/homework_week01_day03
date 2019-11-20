7d8a1149faece6ead64cc9eca2b14d6c477f36d9
Patrick O'Neill <paddyjoneill@hotmail.com>
2019-11-20 17:09:11 +0000

    extensions

---
+++ array_hash_loop_testing_lab/start_point/my_functions.rb
@@ -0,0 +1,25 @@
+def add_array_lengths(array1, array2)
+  return array1.length + array2.length
+end
+
+def sum_array(array)
+  total = 0
+  for number in array
+    total += number
+  end
+  return total
+end
+
+def is_item_in_array(array, item )
+  for number in array
+    if item == number
+      return true
+    end
+  end
+return false
+end
+
+def get_first_key(hash)
+  hash.keys.first
+
+end
